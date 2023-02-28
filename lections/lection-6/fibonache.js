421385af57f20b72e685cf344969a6b53433ab0e
Hanna.Lytvynenko <ani-k-a@ukr.net>
2023-02-28 20:32:50 +0100

    refactoring fibo

--- lections/lection-6/fibonache.js
+++ lections/lection-6/fibonache.js
@@ -3,15 +3,10 @@ function findNumberFibonache(n){
 let arr = [];
 let number;
 for(let i = 0; i < n; i++) {
-    if (i > 1){number = arr[i-2]+ arr[i-1];
-
-    } else {number = 1;}
-    
-    arr.push(number);
-    
+    number = i > 1 ? arr[i-2]+ arr[i-1] : 1;
+    arr.push(number);  
 }
-
-return arr.find((el, ind) => ind === (n-1));
+return arr[n-1];
 }
 
 // findNumberFibonache(index);
